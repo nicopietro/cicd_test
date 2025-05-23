c6a70fa242f076c62b6725db5a0d2b0937e55bfd
nicopietro <nicojpietro@gmail.com>
2025-05-23 18:37:29 +0200

    test

---
+++ upper.py
@@ -0,0 +1,11 @@
+def make_uppercase(x: str) -> str:
+    """
+    Convert a string to uppercase.
+
+    Args:
+        x (str): The string to convert.
+
+    Returns:
+        str: The uppercase version of the input string.
+    """
+    return x.upper()
